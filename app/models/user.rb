df1fe20b889046e85bc99106e40bd137efa98a5b
koji509 <thislifek@yahoo.co.jp>
2020-12-06 20:22:31 +0900

    ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  with_options presence: true do
+    validates :password, format: { with: /\A[-]?[0-9]+(\.[0-9]+)?\z/, message: "Include both letters and numbers" }
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters" }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters" }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters" }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters" }
+    validates :birthday
+  end
+
+  has_many :products
+  has_many :purchases
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"test"}
+    email                 {"test@example"}
+    password              {"000000"}
+    password_confirmation {password}
+    first_name            {"隆"}
+    last_name             {"佐藤"}
+    first_name_kana       {"タカシ"}
+    last_name_kana        {"サトウ"}
+    birthday              {"1980-02-09"}
+  end
+end
