1425edc5e5117f4799c95ba7af7a7044329fd1af
koji509 <thislifek@yahoo.co.jp>
2020-12-07 16:28:26 +0900

    with_options 追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,16 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers" }
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters" }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters" }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters" }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters" }
-    validates :birthday
   end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters" }do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters" }do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+    validates :birthday, presence: true
 
   has_many :products
   has_many :purchases
