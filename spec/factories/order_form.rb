d504fe0338f2bbb76ed545e4bead6c0e49e662ba
koji509 <thislifek@yahoo.co.jp>
2020-12-17 16:58:26 +0900

    db アクティブハッシュ　修正

--- spec/factories/order_form.rb
+++ spec/factories/order_form.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :order_form do
     token         {"tok_abcdefghijk00000000000000000"}
     postal_code   {"123-1234"}
-    prefecture_id {"3"}
+    prefecture_id {3}
     city          {"目黒区"}
     house_number  {"中目黒4-9-10"}
     building_name {"Saison Garden中目黒101"}
